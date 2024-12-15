4c9db7d98f7a0d248cfe51b5b368f2a72c62e74c
M Ryan Bagus Valentine <75926612+MRyanBagusV@users.noreply.github.com>
2024-12-15 21:23:46 +0700

    Update atau menambahkan file tugas_CRUD

--- MRyanValentin/tugas_CRUD/config.php
+++ MRyanValentin/tugas_CRUD/config.php
@@ -13,21 +13,5 @@ if ($conn) { // jika koneksi berhasil
 } else { // jika koneksi gagal
     echo "Not Connected" ; // maka akan tampil "Not Connected"
 }
-// Query untuk menampilkan anggota yang pernah melakukan peminjaman
-$sql = "SELECT a.id_anggota, a.nama, a.telp, p.tgl_pinjam 
-        FROM anggota a 
-        JOIN peminjaman p ON a.id_anggota = p.id_anggota 
-        ORDER BY a.id_anggota ASC"; //berdasarkan id_anggota tampil secara ascending atau dari yang terkecil
-        
-$result = $conn->query($sql); // eksekusi query 
-
-// Hasil Output
-if ($result->num_rows > 0) {// jika ada data 
-    while($row = $result->fetch_assoc()) {// maka akan tampil data dengan while
-        echo "ID: " . $row["id_anggota"]. " - Nama: " . $row["nama"]. " - Telepon: " . $row["telp"] . " - Tanggal Pinjam: " . $row["tgl_pinjam"] . "<br>";
-    }
-} else {
-    echo "0 results"; // jika tidak ada data maka akan tampil "0 results"
-}
 
 ?>
