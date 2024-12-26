eb35c06839ecb6fdae7887cd38d7c52a24ab145c
mamantube <firmanhiday.id007@gmail.com>
2024-12-26 22:33:40 +0700

    add products, cart and function addToCart

--- firman/task-vue/cdn/vue.js
+++ firman/task-vue/cdn/vue.js
@@ -6,56 +6,23 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
-      persons : [
-        {
-            id: 1,
-            name : "Rian",
-            age : 25,
-            address : "Jakarta",
-            point : 70
-        },
-        {
-            id: 2,
-            name : "Alisa",
-            age : 20,
-            address : "Tangerang",
-            point : 80
-        },
-        {  
-            id: 3,
-            name : "Sinta",
-            age : 17,
-            address : "Bandung",
-            point : 60
-        },
-        {
-            id: 4,
-            name : "Joko",
-            age : 30,
-            address : "Jakarta",
-            point : 65
-        },
-        {
-            id: 5,
-            name : "Baskara",
-            age : 35,
-            address : "Tangerang",
-            point : 80
-        },
-        {
-            id: 6,
-            name : "Ratih",
-            age : 27,
-            address : "Bandung",
-            point : 80
-        },
-      ],
-      number : 1,
-      computed: {
-        incrementedNumber() {
-            return ++this.number
-        }
-      }
+        products: null,
+        carts: [],
     };
-  }
+  },
+  methods: {
+    addToCart(item) {
+        this.carts.push(item)
+    }
+  },
+  mounted() {
+    fetch("https://hplussport.com/api/products/order/price")
+    .then((res) => res.json())
+    .then(data => {
+        this.products = data;
+    })
+    .catch((err) => {
+        console.error("failed to fetch products", err)
+    })
+    },
 }).mount("#app");
