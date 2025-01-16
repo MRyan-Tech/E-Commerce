7b20f3b11dd14a79aa6aa9534497faa5af8dfc9d
mamantube <firmanhiday.id007@gmail.com>
2025-01-16 11:10:30 +0700

    update field and add relation

--- firman/task-laravel/final-task/database/migrations/2025_01_15_082140_create_transactions_table.php
+++ firman/task-laravel/final-task/database/migrations/2025_01_15_082140_create_transactions_table.php
@@ -13,6 +13,21 @@ return new class extends Migration
     {
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
+            $table->string("order_id");
+            $table->integer("gross_amount");
+            $table->integer("qty_order");
+            $table->string("transaction_id")->default(null);
+            $table->string("token");
+            $table->string("pending");
+            $table->foreignId("user_id")->constrained(
+                table: "users", indexName: "transactions_user_id"
+            );
+            $table->foreignId("product_id")->constrained(
+                table: "products", indexName: "transactions_product_id"
+            );
+            $table->string("note_refund")->default(null);
+            $table->date("refund_date")->default(null);
+            $table->date("deleted_at")->default(null);
             $table->timestamps();
         });
     }
