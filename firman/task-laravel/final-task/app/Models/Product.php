b9465cd09db13cc4408f9d619eb9025bd40ac3e6
mamantube <firmanhiday.id007@gmail.com>
2025-01-16 10:15:28 +0700

    update and fixing code

--- firman/task-laravel/final-task/app/Models/Product.php
+++ firman/task-laravel/final-task/app/Models/Product.php
@@ -9,6 +9,9 @@ class Product extends Model
     protected $fillable = [
         "name",
         "price",
+        "category",
         "description",
+        "qty",
+
     ];
 }
---
+++ firman/task-laravel/final-task/database/migrations/0001_01_01_000001_create_storages_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('storages', function (Blueprint $table) {
+            $table->id();
+            $table->string("public_id");
+            $table->string("secure_url");
+            $table->date("deteleted_at")->default(null);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('storages');
+    }
+};
