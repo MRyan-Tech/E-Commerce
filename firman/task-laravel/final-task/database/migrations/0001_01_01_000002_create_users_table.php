e3d7b85b1507d098316bc8c5f5f57fd630072eff
mamantube <firmanhiday.id007@gmail.com>
2025-01-23 12:21:46 +0700

    update

--- firman/task-laravel/final-task/database/migrations/0001_01_01_000002_create_users_table.php
+++ firman/task-laravel/final-task/database/migrations/0001_01_01_000002_create_users_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('first_name');
-            $table->string('last_name');
+            $table->string('name');
+            $table->string('user_name');
             $table->integer('phone');
             $table->string('email')->unique();
             $table->boolean('is_admin')->default(false);
