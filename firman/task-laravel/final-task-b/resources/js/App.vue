de5a31e55b14eb7da20e19e9c281d7d91f4bd3bd
mamantube <firmanhiday.id007@gmail.com>
2025-02-04 12:45:21 +0700

    add an config project with vue and boostrap

---
+++ firman/task-laravel/final-task-b/resources/js/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+const sayHello = () => {
+    alert("Vue js dan bootstrap sudah terpasang");
+};
+</script>
+
+<template>
+    <div class="container mt-5">
+        <h1 class="text-primary">Halo dari Vue + Bootstrap!</h1>
+        <button class="btn btn-success" @click="sayHello">Klik Saya</button>
+    </div>
+</template>
