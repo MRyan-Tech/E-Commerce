ee2ea1b1ded1867234c8fff284eb4bcd76d4c790
mamantube <firmanhiday.id007@gmail.com>
2025-03-17 15:47:37 +0700

    tidy up file

--- firman/task-laravel/final-task-b/routes/api.php
+++ firman/task-laravel/final-task-b/routes/api.php
@@ -11,31 +11,15 @@ Route::post("/login", [UserController::class, "login"]);
 Route::get('/users', [UserController::class, 'allUser']);
 Route::patch("/users/{id}", [UserController::class, "updateUser"]);
 Route::get("/users/{id}", [UserController::class, "user"]);
-Route::post("/products", [ProductController::class, "addProduct"]);
 Route::get("products/{id}", [ProductController::class, "product"]);
-Route::patch("products/{id}", [ProductController::class, "updateProduct"]);
+Route::get("/products", [ProductController::class, "allProducts"]);
 
 
 Route::middleware(['auth:sanctum'])->group(function () {
-    Route::get("/products", [ProductController::class, "allProducts"]);
-
+    
     Route::prefix('products')->group(function () {
-        // Route::get('/', [ProductController::class, 'data']);
-        Route::post('/', [ProductController::class, 'store']);
-        // Route::get('/{id}', [ProductController::class, 'show']);
-        // Route::put('/{id}', [ProductController::class, 'update']);
-        // Route::delete('/{id}', [ProductController::class, 'destroy']);
+        Route::post("/add", [ProductController::class, "addProduct"]);
+        Route::post("/update/{id}", [ProductController::class, "updateProduct"]);
     });
 
 });
-
-
-// Route yang memerlukan autentikasi
-// Route::middleware(['auth:sanctum'])->group(function () {
-//     Route::get("/users", [UserController::class, "allUser"]);   
-
-//     Route::prefix("product")->group(function () {
-//         Route::get("/", [ProductController::class, "allProducts"]);
-//         Route::post("/addProduct", [ProductController::class, "addProduct"]);
-//     });
-// });
