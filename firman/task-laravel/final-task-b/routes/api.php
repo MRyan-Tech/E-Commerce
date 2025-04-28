d8065901fcaee5219498777b7f321bf95a60791b
mamantube <firmanhiday.id007@gmail.com>
2025-04-28 18:13:07 +0700

    add delete product route api

--- firman/task-laravel/final-task-b/routes/api.php
+++ firman/task-laravel/final-task-b/routes/api.php
@@ -2,10 +2,8 @@
 
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Route untuk registrasi dan login
 Route::post("/register", [UserController::class, "register"]);
 Route::post("/login", [UserController::class, "login"]);
 Route::get('/users', [UserController::class, 'allUser']);
@@ -16,10 +14,10 @@ Route::get("/products", [ProductController::class, "allProducts"]);
 
 
 Route::middleware(['auth:sanctum'])->group(function () {
-    
     Route::prefix('products')->group(function () {
         Route::post("/add", [ProductController::class, "addProduct"]);
         Route::post("/update/{id}", [ProductController::class, "updateProduct"]);
+        Route::delete("/{id}", [ProductController::class, "deleteProduct"]);
     });
 
 });
