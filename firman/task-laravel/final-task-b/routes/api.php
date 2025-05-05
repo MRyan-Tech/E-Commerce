2c5556e54c843c2c78a37704ad3bd37aac031f2d
mamantube <firmanhiday.id007@gmail.com>
2025-05-05 16:04:21 +0700

    tidy up and add delete user route api

--- firman/task-laravel/final-task-b/routes/api.php
+++ firman/task-laravel/final-task-b/routes/api.php
@@ -6,7 +6,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::post("/register", [UserController::class, "register"]);
 Route::post("/login", [UserController::class, "login"]);
-Route::get('/users', [UserController::class, 'allUser']);
 Route::patch("/users/{id}", [UserController::class, "updateUser"]);
 Route::get("/users/{id}", [UserController::class, "user"]);
 Route::get("products/{id}", [ProductController::class, "product"]);
@@ -14,6 +13,9 @@ Route::get("/products", [ProductController::class, "allProducts"]);
 
 
 Route::middleware(['auth:sanctum'])->group(function () {
+    
+    Route::get("/users", [UserController::class, "allUser"]);
+    Route::delete("/users/{id}", [UserController::class, "deleteUser"]);
     Route::prefix('products')->group(function () {
         Route::post("/add", [ProductController::class, "addProduct"]);
         Route::post("/update/{id}", [ProductController::class, "updateProduct"]);
